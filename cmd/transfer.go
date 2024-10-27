eab3834139f70c106d9becc290899c908adbd704
Riley Flynn <riley@rileyflynn.me>
2024-10-27 14:43:35 -0230

    Transfer prompting; No actual transfer yet

--- cmd/transfer.go
+++ cmd/transfer.go
@@ -1,7 +1,15 @@
 package cmd
 
 import (
+	"context"
+
+	"github.com/charmbracelet/huh"
+	gh "github.com/cli/go-gh/v2/pkg/api"
+	"github.com/google/go-github/v66/github"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
+
+	"github.com/nint8835/repo-archiver/pkg/config"
 )
 
 var transferCmd = &cobra.Command{
@@ -9,7 +17,57 @@ var transferCmd = &cobra.Command{
 	Short: "Transfer repos between accounts",
 	Args:  cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
+		var sourceAccount string
+		var destinationAccount string
+		var repository string
 
+		var accountOptions []huh.Option[string]
+		for displayName := range config.Instance.Accounts {
+			accountOptions = append(accountOptions, huh.NewOption(displayName, displayName))
+		}
+
+		authenticatedClient, err := gh.DefaultHTTPClient()
+		checkError(err, "error creating authenticated client")
+
+		ghClient := github.NewClient(authenticatedClient)
+
+		repositoryOptionsFunc := func() []huh.Option[string] {
+			var repositoryOptions []huh.Option[string]
+
+			repos, _, err := ghClient.Repositories.ListByUser(
+				context.Background(),
+				config.Instance.Accounts[sourceAccount].Name,
+				nil,
+			)
+			if err != nil {
+				log.Warn().Err(err).Msg("error listing repositories")
+				return repositoryOptions
+			}
+
+			for _, repo := range repos {
+				repositoryOptions = append(repositoryOptions, huh.NewOption(*repo.Name, *repo.Name))
+			}
+
+			return repositoryOptions
+		}
+
+		err = huh.NewForm(
+			huh.NewGroup(
+				huh.NewSelect[string]().
+					Options(accountOptions...).
+					Title("Source Account").
+					Value(&sourceAccount),
+				huh.NewSelect[string]().
+					Options(accountOptions...).
+					Title("Destination Account").
+					Value(&destinationAccount),
+				huh.NewSelect[string]().
+					OptionsFunc(repositoryOptionsFunc, &sourceAccount).
+					Title("Repository").
+					Value(&repository),
+			),
+		).WithTheme(huh.ThemeCatppuccin()).Run()
+		checkError(err, "error prompting for transfer information")
 	},
 }
 
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.23.2
 
 require (
 	github.com/charmbracelet/huh v0.6.0
+	github.com/cli/go-gh/v2 v2.11.0
+	github.com/google/go-github/v66 v66.0.0
 	github.com/pterm/pterm v0.12.79
 	github.com/rs/zerolog v1.33.0
 	github.com/spf13/cobra v1.8.1
@@ -23,10 +25,14 @@ require (
 	github.com/charmbracelet/x/ansi v0.4.0 // indirect
 	github.com/charmbracelet/x/exp/strings v0.0.0-20241025155609-902b1d1de0be // indirect
 	github.com/charmbracelet/x/term v0.2.0 // indirect
+	github.com/cli/safeexec v1.0.1 // indirect
+	github.com/cli/shurcooL-graphql v0.0.4 // indirect
 	github.com/containerd/console v1.0.4 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/gookit/color v1.5.4 // indirect
+	github.com/henvic/httpretty v0.1.4 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/lithammer/fuzzysearch v1.1.8 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
@@ -40,6 +46,7 @@ require (
 	github.com/muesli/termenv v0.15.3-0.20240618155329-98d742f6907a // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/thlib/go-timezone-local v0.0.3 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
